8942531e36286bd5d7a75cc7fbb9c2df742eeffd
RFBS18 <fabriziobarrios22@gmail.com>
2024-01-25 14:48:32 -0500

    agregando diseño dashboard

---
+++ PresentacionAdmin/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PresentacionAdmin
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new Bundle("~/bundles/complementos").Include(
+                "~/Scripts/bootstrap.bundle.min.js",
+                "~/Scripts/jquery.min.js",
+                "~/Scripts/sb-admin-2.min.js"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            // bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/sb-admin-2.min.css",
+                      "~/Content/all.min.css"));
+        }
+    }
+}
